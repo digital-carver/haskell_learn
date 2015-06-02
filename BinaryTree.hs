16a3d1f349e8f7335bca25da84d4b4904f258c26
SundaraRaman R <sundar.vignyaan@gmail.com>
2015-06-02 22:06:35 +0530

    basic binary tree with treeSum

---
+++ BinaryTree.hs
@@ -0,0 +1,17 @@
+module BinaryTree 
+
+(Tree(..), treeSum)
+
+where
+
+data Tree = Tree Int Tree Tree 
+          | Leaf Int 
+          | NullNode
+          deriving (Show)
+
+treeSum :: Tree -> Int 
+treeSum NullNode = 0
+treeSum (Leaf val) = val 
+treeSum (Tree val left right) = val + (treeSum left) + (treeSum right) 
+
+
